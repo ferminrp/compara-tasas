86353598b663db9bbd96b06ade0cbcf4905d8250
ferminrp <frodriguezpenelas@gmail.com>
2024-02-08 21:35:48 -0300

    partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), partytown()]
 });
