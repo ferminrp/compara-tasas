da42166e03165d8217a8c782d37d32ce4486bfc3
ferminrp <frodriguezpenelas@gmail.com>
2024-02-08 21:36:49 -0300

    sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown(), sitemap()]
 });
