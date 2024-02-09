df85f19decb44a872a206c3d6c29235f8ea84953
ferminrp <frodriguezpenelas@gmail.com>
2024-02-09 19:26:37 -0300

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown(), sitemap()]
+  integrations: [tailwind(), partytown(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
