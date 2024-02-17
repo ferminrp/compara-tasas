51a3c287121037e96e6f6712e89f3ff4ea1db93a
fgadda <facugadda@gmail.com>
2024-02-17 20:57:32 -0300

    add utils to handle percentage and currency formats

---
+++ src/utils/formats.ts
@@ -0,0 +1,11 @@
+export const formatPercentage = (percentage: number): string =>
+  new Intl.NumberFormat('es-AR', {
+    style: 'percent',
+    minimumFractionDigits: 2
+  }).format(percentage / 100)
+
+export const formatCurrency = (amount: number): string =>
+  new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS'
+  }).format(amount)
