f22dc4c1b188d64f40448e6a6b9c57dc77fd1d0f
AdrielIg <adrielgomezignacio@gmail.com>
2024-02-16 22:42:59 -0300

    add prettier and linter

--- src/model/business.ts
+++ src/model/business.ts
@@ -1,18 +1,24 @@
 export type Rate = 'TNA' | 'TEA'
 
-export type InvestmentType = "cuenta_remunerada" | "tradicional" | "pf" | "criptopesos" | "buendolar" | "fiwind"
+export type InvestmentType =
+  | 'cuenta_remunerada'
+  | 'tradicional'
+  | 'pf'
+  | 'criptopesos'
+  | 'buendolar'
+  | 'fiwind'
 
 export interface Investment {
-    id?: number,
-    name: string,
-    tasa_diaria: number,
-    logo: string,
-    display: boolean,
-    full_liquidity: boolean,
-    url: string,
-    type: InvestmentType,
-    detail: string,
-    tna: number,
-    tea: number,
-    title?:string
-  }
+  id?: number
+  name: string
+  tasa_diaria: number
+  logo: string
+  display: boolean
+  full_liquidity: boolean
+  url: string
+  type: InvestmentType
+  detail: string
+  tna: number
+  tea: number
+  title?: string
+}
