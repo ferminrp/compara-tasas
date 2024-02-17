a4db0e5691a612653879845c096b5238a6278843
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2024-02-17 08:17:30 -0300

    Update argentinaDatos.ts
    sumo logos

--- src/client/argentinaDatos.ts
+++ src/client/argentinaDatos.ts
@@ -26,17 +26,35 @@ export const getReturnsByApp = async (app: App): Promise<EnrichedReturn[]> => {
 const MAPPING_RETURNS = {
   // Missing logos
   // NARS: {}
-  // ADA:{}
-  // BTC
-  // ETH
-  // DOT
-  // MATIC
-  // BNB
-  // NUARS
-  // AVAX
   // NUPEN
   // NCOP
-  // USDM
+  ADA: {
+    logo: 'https://ik.imagekit.io/ferminrp/ada.png?updatedAt=1708168609471'
+  },
+  AVAX: {
+    logo: 'https://ik.imagekit.io/ferminrp/avax.png?updatedAt=1708168433703'
+  },
+  BTC: {
+    logo: 'https://ik.imagekit.io/ferminrp/Bitcoin.svg.png?updatedAt=1703766672941'
+  },
+  ETH: {
+    logo: 'https://ik.imagekit.io/ferminrp/eth-logo?updatedAt=1708168133038'
+  },
+  DOT: {
+    logo: 'https://ik.imagekit.io/ferminrp/dot.png?updatedAt=1708168304391'
+  },
+  MATIC: {
+    logo: 'https://ik.imagekit.io/ferminrp/matic.webp?updatedAt=1708167991700'
+  },
+  BNB: {
+    logo: 'https://ik.imagekit.io/ferminrp/download.png?updatedAt=1703518180297'
+  },
+  NUARS: {
+    logo: 'https://ik.imagekit.io/ferminrp/nuars.webp?updatedAt=1706442438735'
+  },
+  USDM: {
+    logo: 'https://ik.imagekit.io/ferminrp/usdm.webp?updatedAt=1706873664159'
+  },
   DAI: {
     logo: 'https://ik.imagekit.io/ferminrp/dai.png?updatedAt=1706873664303'
   },
