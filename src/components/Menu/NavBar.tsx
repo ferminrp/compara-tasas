171e81f5a721581208bbafdb95c049abba654385
Facundo Masino <facujmasino@gmail.com>
2024-02-23 16:37:52 -0300

    Fix navbar padding

--- src/components/Menu/NavBar.tsx
+++ src/components/Menu/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div className='w-full mx-auto sm:px-4 sm:max-w-sm md:max-w-md lg:max-w-lg'>
       <div className='py-4'>
-        <div className='border-l-[3px] border-indigo-500 text-neutral-900 dark:text-white flex items-center justify-between px-6'>
+        <div className='border-l-[3px] border-indigo-500 text-neutral-900 dark:text-white flex items-center justify-between px-4 sm:pl-4 sm:pr-0'>
           <div className='flex flex-col font-semibold'>
             <span>COMPARA</span>
             <span>TASAS</span>
