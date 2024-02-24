dfe347d07bd8b772a94cb65e1b5938639c7eb9e8
facumasino <facujmasino@gmail.com>
2024-02-24 18:43:48 -0300

    Add svg logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,35 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import svgr from 'vite-plugin-svgr'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), partytown(), sitemap(), react()],
   site: 'https://comparatasas.ar',
-  output: "server",
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    plugins: [
+      svgr({
+        include: '**/*.svg?react',
+        svgrOptions: {
+          plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+          svgoConfig: {
+            plugins: [
+              'preset-default',
+              'removeTitle',
+              'removeDesc',
+              'removeDoctype',
+              'cleanupIds'
+            ]
+          }
+        }
+      })
+    ]
+  }
+})
--- src/components/Menu/NavBar.tsx
+++ src/components/Menu/NavBar.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Drawer from '../Drawer'
 import MenuDropdown from './MenuDropdown'
+// @ts-ignore
+import LogoLight from '../../assets/logo-light.svg?react'
+// @ts-ignore
+import LogoDark from '../../assets/logo-dark.svg?react'
 
 const NavBar = ({
   pathname
@@ -13,10 +17,11 @@ const NavBar = ({
     <div className='w-full mx-auto sm:px-4 sm:max-w-sm md:max-w-md lg:max-w-lg'>
       <div className='py-4'>
         <div className='border-l-[3px] border-indigo-500 text-neutral-900 dark:text-white flex items-center justify-between px-4 sm:pl-4 sm:pr-0'>
-          <div className='flex flex-col font-semibold'>
-            <span>COMPARA</span>
-            <span>TASAS</span>
-            <span className='dark:text-gray-400'>.AR</span>
+          <div className='hidden dark:block'>
+            <LogoDark />
+          </div>
+          <div className='dark:hidden'>
+            <LogoLight />
           </div>
           <MenuDropdown pathname={pathname} />
         </div>
