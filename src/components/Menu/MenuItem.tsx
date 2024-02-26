4fe9ad1b14461f5c9db5300dee2edf0be58ff68d
santigandolfo <santiago.gandolfo@tiendanube.com>
2024-02-26 10:54:20 -0300

    Lit code

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import type { MenuItemType } from '../../model/menu'
+import React from 'react';
+import type { MenuItemType } from '../../model/menu';
 
 const MenuItem = ({
   active,
-  data
+  data,
 }: {
   /** Determina si el item esta activo o no en el momento **/
-  active: boolean
+  active: boolean;
   /** Contiene un objecto ItemDataType con los datos del item  */
-  data: MenuItemType
+  data: MenuItemType;
 }) => {
-  const { hasTopBorder, url, icon, name } = data
+  const { hasTopBorder, url, icon, name } = data;
   return (
     <>
       {hasTopBorder && (
-        <hr className='bg-[#CAD0E0] dark:bg-[#292B2E] w-full h-px border-0' />
+        <hr className="bg-[#CAD0E0] dark:bg-[#292B2E] w-full h-px border-0" />
       )}
       <li
-        className={`${active && 'bg-indigo-500'} w-full ${!active && 'hover:bg-gray-100 dark:hover:bg-gray-950'} group`}
+        className={`${active && 'bg-indigo-500'} w-full ${
+          !active && 'hover:bg-gray-100 dark:hover:bg-gray-950'
+        } group`}
       >
         <a
-          className={`${active ? 'text-white' : 'text-[#6E727A]'} px-6 py-3 block font-semibold ${!active && 'group-hover:text-[#6E727A]'}`}
+          className={`${
+            active ? 'text-white' : 'text-[#6E727A]'
+          } px-6 py-3 block font-semibold ${
+            !active && 'group-hover:text-[#6E727A]'
+          }`}
           href={url}
         >
-          <span className='pr-5'>{icon}</span> {name}
+          <span className="pr-5">{icon}</span> {name}
         </a>
       </li>
     </>
-  )
-}
+  );
+};
 
-export default MenuItem
+export default MenuItem;
--- src/components/Menu/NavBar.tsx
+++ src/components/Menu/NavBar.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import MenuDropdown from './MenuDropdown'
-import Logo from '../../assets/Logo'
+import React from 'react';
+import Logo from '../../assets/Logo';
+import MenuDropdown from './MenuDropdown';
 
 const NavBar = ({
-  pathname
+  pathname,
 }: {
   /** Utiliza la ruta actual para indicar
    * cual item del menu está activo **/
-  pathname: string
+  pathname: string;
 }) => {
   return (
-    <div className='w-full mx-auto sm:px-4 sm:max-w-sm md:max-w-md lg:max-w-lg'>
-      <div className='py-4'>
-        <div className='border-l-[3px] border-indigo-500 text-neutral-900 dark:text-white flex items-center justify-between px-4 sm:pl-4 sm:pr-0'>
-          <div className='hidden dark:block'>
+    <div className="w-full mx-auto sm:px-4 sm:max-w-sm md:max-w-md lg:max-w-lg">
+      <div className="py-4">
+        <div className="border-l-[3px] border-indigo-500 text-neutral-900 dark:text-white flex items-center justify-between px-4 sm:pl-4 sm:pr-0">
+          <div className="hidden dark:block">
             <Logo darkMode />
           </div>
-          <div className='dark:hidden'>
+          <div className="dark:hidden">
             <Logo />
           </div>
           <MenuDropdown pathname={pathname} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
