586fc8add775df0e1e534d2128a9330edabda73e
Gustavo Smith <gustavosmith0010@gmail.com>
2024-02-25 22:26:32 -0300

    Lock de dependencias + cambio de nombre en package.json + instalación de prettier-plugin-tailwindcss

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,16 +1,19 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-    // i am just using the standard config, change if you need something else
-    ...require('prettier-config-standard'),
-    pluginSearchDirs: [__dirname],
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-        {
-            files: '*.astro',
-            options: {
-                parser: 'astro',
-                printWidth: 70
-            }
-        }
-    ]
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  pluginSearchDirs: [__dirname],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    'prettier-plugin-tailwindcss'
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+        printWidth: 70
+      }
+    }
+  ]
 }
