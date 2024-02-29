1f29d32b727af43440254218301a4ec19216c192
AdrielIg <adrielgomezignacio@gmail.com>
2024-02-29 09:08:44 -0300

    make app static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), partytown(), sitemap(), react()],
   site: 'https://comparatasas.ar',
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercelStatic(),
 });
