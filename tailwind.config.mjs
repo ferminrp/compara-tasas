8c4ea1d7866e99d4a60299d1e300f8c0580b47de
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2024-03-16 19:08:17 -0300

    vuelvo a la font original

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter'],
-      },
+      fontFamily: {},
     },
   },
   plugins: [],
