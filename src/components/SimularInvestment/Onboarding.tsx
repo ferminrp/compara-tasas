3525693853e9916b2d5bfcd36f6ad5d7bd7cbee3
fgadda <facugadda@gmail.com>
2024-04-07 20:11:06 -0300

    remove close button

--- src/components/SimularInvestment/Onboarding.tsx
+++ src/components/SimularInvestment/Onboarding.tsx
@@ -36,24 +36,6 @@ interface SimulacionOnboardingProps {
   multiplo30: boolean;
 }
 
-const CloseButton = ({ isDarkMode }: { isDarkMode: boolean }) => {
-  return (
-    <Button variant='ghost' className='pr-0 hover:!bg-transparent'>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='16'
-        height='16'
-        className='bi bi-x-lg iconClose'
-        viewBox='0 0 16 16'
-        fill={isDarkMode ? 'white' : 'black'}
-      >
-        <title>Cerrar</title>
-        <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z' />
-      </svg>
-    </Button>
-  );
-};
-
 const Dot = ({ isStepActive }: { isStepActive: boolean }) => {
   return (
     <div
@@ -128,7 +110,19 @@ export default function SimulacionOnboarding({
           <Dot isStepActive={isStepActive(2)} />
         </div>
         <DialogClose asChild>
-          <CloseButton isDarkMode={isDarkMode} />
+          <Button variant='ghost' className='pr-0 hover:!bg-transparent'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              className='bi bi-x-lg iconClose'
+              viewBox='0 0 16 16'
+              fill={isDarkMode ? 'white' : 'black'}
+            >
+              <title>Cerrar</title>
+              <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z' />
+            </svg>
+          </Button>
         </DialogClose>
       </div>
 
