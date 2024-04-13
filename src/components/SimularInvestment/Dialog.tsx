ce7c895b9271255e10253c1f2406b867825e564f
Adriel Gomez <75552918+AdrielIg@users.noreply.github.com>
2024-04-13 18:06:32 -0300

    Update Dialog.tsx

--- src/components/SimularInvestment/Dialog.tsx
+++ src/components/SimularInvestment/Dialog.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import SimulacionOnboarding from './Onboarding';
 import { useMediaQuery } from './hooks/use-media-query';
-import { Button } from './ui/button';
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 import { Drawer, DrawerContent, DrawerTrigger } from './ui/drawer';
+import { Button } from './ui/button';
+import SimulacionOnboarding from './Onboarding';
 
 export default function SimularInversion({ data, multiplo30 }) {
   const [open, setOpen] = React.useState(false);
