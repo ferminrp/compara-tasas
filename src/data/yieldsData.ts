7cdb04f6935ea4fd57aa8637d1e911675dca6ae4
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2024-04-21 16:48:44 -0300

    logos de chains

--- src/data/yieldsData.ts
+++ src/data/yieldsData.ts
@@ -17,7 +17,37 @@ export const PROTOCOLS: Protocols = {
 export const BLOCKCHAINS: Blockchains = {
   eth: {
     name: 'Ethereum',
-    logo: 'https://ik.imagekit.io/ferminrp/gKj3U76V_400x400.jpg?updatedAt=1702030494696',
+    logo: 'https://ik.imagekit.io/ferminrp/eth-logo?updatedAt=1708168133038',
     altLogo: 'Ethereum Logo',
+  },
+  arb: {
+    name: 'Arbitrum',
+    logo: 'https://ik.imagekit.io/ferminrp/arbitrum.webp?updatedAt=1713728759449',
+    altLogo: 'Arbitrum Logo',
+  },
+  poly: {
+    name: 'Polygon',
+    logo: 'https://ik.imagekit.io/ferminrp/matic.webp?updatedAt=1708167991700',
+    altLogo: 'Polygon Logo',
+  },
+  gnosis: {
+    name: 'Gnosis',
+    logo: 'https://ik.imagekit.io/ferminrp/gKj3U76V_400x400.jpg?updatedAt=1702030494696',
+    altLogo: 'Gnosis Logo',
+  },
+  base: {
+    name: 'Base',
+    logo: 'https://ik.imagekit.io/ferminrp/base.png?updatedAt=1713728828750',
+    altLogo: 'Base Logo',
+  },
+  avax: {
+    name: 'Avalanche',
+    logo: 'https://ik.imagekit.io/ferminrp/avax.png?updatedAt=1708168433703',
+    altLogo: 'Avalanche Logo',
+  },
+  opt: {
+    name: 'Optimism',
+    logo: 'https://ik.imagekit.io/ferminrp/optimism-ethereum-op-logo.png?updatedAt=1703511056838',
+    altLogo: 'Optimism Logo',
   },
 };
