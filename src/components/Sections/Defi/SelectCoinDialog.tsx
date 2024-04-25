ab7bb536f1c31113791c067c2a5a773a163e2b58
AdrielIg <adrielgomezignacio@gmail.com>
2024-04-24 21:55:42 -0300

    delete comments

--- src/components/Sections/Defi/SelectCoinDialog.tsx
+++ src/components/Sections/Defi/SelectCoinDialog.tsx
@@ -61,7 +61,7 @@ const ContentDialog = ({
           pocketShadowLight: !isDarkMode,
         })}
       >
-        {[...options]
+        {options
           .sort((a, b) => b.apy - a.apy)
           .map((item) => {
             return (
